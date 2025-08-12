4e3789ed890ede4a100da23657a7b47e15d706b8
Cao Viet <caoduongvietquoc@gmail.com>
2025-08-12 17:01:06 +0700

    'validate'

--- Views/auth/register.php
+++ Views/auth/register.php
@@ -210,21 +210,7 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-    // Không cần xử lý chọn role vì chỉ có patient
-
-    // Validation password
-    document.getElementById('confirm_password').addEventListener('input', function() {
-        const password = document.getElementById('password').value;
-        const confirmPassword = this.value;
-
-        if (password !== confirmPassword) {
-            this.setCustomValidity('Mật khẩu xác nhận không khớp!');
-        } else {
-            this.setCustomValidity('');
-        }
-    });
-    </script>
+    <script src="././assets/js/validate.js"></script>
 </body>
 
 </html>
