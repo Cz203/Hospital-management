f84ba2ca9f534a22b85c3081d4275dd305a08b67
TruongThinh1608 <untt1608@gmail.com>
2025-10-25 07:56:13 +0700

    done merge code nhanh Thinh

--- Views/auth/login_sieuam.php
+++ Views/auth/login_sieuam.php
@@ -34,14 +34,7 @@
                     <label for="phone" class="form-label">
                         <i class="fas fa-phone"></i> Số điện thoại
                     </label>
-<<<<<<< HEAD
-
-                    <input type="tel" class="form-control" id="phone" name="phone" placeholder="Nhập số điện thoại"
-                        required>
-
-=======
                     <input type="tel" class="form-control" id="phone" name="phone" placeholder="Nhập số điện thoại" required>
->>>>>>> b62e60f (hmm)
                 </div>
 
                 <div class="mb-3">
